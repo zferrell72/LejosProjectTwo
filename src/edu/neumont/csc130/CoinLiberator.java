cb4dcfb7ff9847dda65cff87fd8958e54fc39006
sinhpah <zacharyhutcherson@gmail.com>
2013-12-02 21:13:08 -0700

    Update CoinLiberator.java
    Implemented the releaseCoins method :3

--- src/edu/neumont/csc130/CoinLiberator.java
+++ src/edu/neumont/csc130/CoinLiberator.java
@@ -2,8 +2,13 @@ package edu.neumont.csc130;
 
 public class CoinLiberator {
 
+
 //	create new motorController
-//
+	MotorController motorCtrlr = new MotorController();
+	
 //	releaseCoins(){
 //		motorController.releaseCoins();
+	public void releaseCoins(){
+		motorCtrlr.releaseCoins();
+	}
 }
