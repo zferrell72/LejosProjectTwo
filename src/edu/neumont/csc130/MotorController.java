3a9194b3ed23362ae8665236613629cd49728aba
ZFerrell72 <zferrell@student.neumont.edu>
2013-12-02 21:17:45 -0700

    Update MotorController.java

--- src/edu/neumont/csc130/MotorController.java
+++ src/edu/neumont/csc130/MotorController.java
@@ -2,16 +2,56 @@ package edu.neumont.csc130;
 
 public class MotorController {
 
-//	insertCoin(){
-//		rotate motor forward at low power for short duration
-//	}
-//	
-//	returnCoin(){
-//		rotate motor backward at high power for short duration
-//	}
-//
-//	releaseCoins(){
-//		rotate motor forward at low power for long duration
-//	}
+package edu.neumont.csc130;
+
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
+
+public class MotorController {
+  
+  private NXTMotor coinSlotMotor;
+  private NXTMotor doorMotor;
+  private final int insertPower = 40;
+  private final int returnPower = 100;
+  private final int doorPower = 20;
+  
+  
+  public MotorController(){
+      coinSlotMotor = new NXTMotor(MotorPort.A);
+      doorMotor = new NXTMotor(MotorPort.B);
+  }
+
+  public void insertCoin(){
+    coinSlotMotor.setPower(insertPower);
+    coinSlotMotor.forward();
+    try{
+      Thread.sleep(1000);
+    }
+    coinSlotMotor.stop();
+  }
+  
+  public void returnCoin(){
+    coinSlotMotor.setPower(returnPower);
+    coinSlotMotor.backward();
+    try{
+      Thread.sleep(1000);
+    }
+    coinSlotMotor.stop();
+  }
+  
+  public void releaseCoins(){
+    doorMotor.setPower(doorPower);
+    doorMotor.forward();
+    try{
+      Thread.sleep(1000);
+    }
+    doorMotor.backward();
+    try{
+      Thread.sleep(1000);
+    }
+    doorMotor.stop();
+  }
+}
+
 
 }
