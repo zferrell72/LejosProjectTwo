5eff556f57bc91c280fe66902df5a3a61526f256
ZFerrell72 <zferrell@student.neumont.edu>
2013-12-02 20:47:08 -0700

    Finished CoinSlot

--- src/edu/neumont/csc130/CoinSlot.java
+++ src/edu/neumont/csc130/CoinSlot.java
@@ -1,25 +1,35 @@
 package edu.neumont.csc130;
 
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+
 public class CoinSlot {
 
 //	NXTMotor motor
+	private MotorController motor = new MotorController();
 //	lightSensor sensor
-//	
-//	public void calibrateSensor(){
+	private LightSensor sensor = new LightSensor(SensorPort.S1);
+	
+	public void calibrateSensor(){
 //		calibrate the sensor for when there is not a coin
-//	}
-//
-//	public boolean hasCoin(){
+		sensor.calibrateHigh();
+	}
+	
+
+	public boolean hasCoin(){
 //		return if the sensor detects a coin in the CoinSlot
-//	}
-//
-//	public void ejectCoin(){
+		return (sensor.readValue() < sensor.getHigh());
+	}
+
+	public void ejectCoin(){
 //		Set the motor’s power to ejectSpeed
 //		tell the motor to turn backward
-//	}
-//
-//	public void eatCoin(){
+		motor.returnCoin();
+	}
+	
+	public void eatCoin(){
 //		Set the motor’s power to takeCoinSpeed
 //		Tell the motor to turn forward
-//	}
+		motor.insertCoin();
+	}
 }
